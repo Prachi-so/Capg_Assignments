dc2cda79ac8730d09a6bce90e031624d199387e0
Prachi Soni <121783427+Prachi-so@users.noreply.github.com>
2026-03-14 00:10:23 +0530

    Delete onlineBookOrderingSystem_caseStudy directory

--- onlineBookOrderingSystem_caseStudy/order-service/src/main/java/com/lpu/order_service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lpu.order_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.lpu.order_service.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer>{
-
-}
